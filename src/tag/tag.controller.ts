c13f3ffe88783738c16572311dc856fc05fc36ee
Pavlik1996 <miki2012miki@gmail.com>
2023-07-16 20:35:56 +0300

    lesson 5

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,9 +1,11 @@
+import { TagService } from './tag.service';
 import { Controller, Get } from '@nestjs/common';
 
 @Controller('tags')
 export class TagController {
+  constructor(private readonly tagService: TagService) {}
   @Get()
-  findAll() {
-    return ['dragons', 'coffee'];
+  findAll(): string[] {
+    return this.tagService.findAll();
   }
 }
